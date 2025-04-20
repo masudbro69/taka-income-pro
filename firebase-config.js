c5bf9309f4f66f53b1664fa62d048fda6a15724b
masudbro69 <officialmasudbro@gmail.com>
2025-04-20 12:38:21 +0600

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDF1UgHvIhWZhjeznq5d3nQRPxKYltZHg8",
+  authDomain: "taka-income-pro.firebaseapp.com",
+  projectId: "taka-income-pro",
+  storageBucket: "taka-income-pro.appspot.com",
+  messagingSenderId: "278259025169",
+  appId: "1:278259025169:web:a7c43070194c80465ff0cd"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
