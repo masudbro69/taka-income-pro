e6bb84972cab513bf9b3fc1a2206785f8802ddc8
masudbro69 <officialmasudbro@gmail.com>
2025-04-20 20:01:22 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,15 @@
-// Firebase configuration
+// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCqjGx0X5sAFaD5vTrmuEkOX_aNNfHBEBo",
+  apiKey: "AIzaSyDts5_NDqvHpgZIBX-GzNij28UHoULr8_Y",
   authDomain: "taka-income-pro.firebaseapp.com",
   projectId: "taka-income-pro",
   storageBucket: "taka-income-pro.appspot.com",
-  messagingSenderId: "94213173931",
-  appId: "1:94213173931:web:8583e617f2b14d2e2e4ac0"
+  messagingSenderId: "153366647999",
+  appId: "1:153366647999:web:fb63b29c2a2a9ce93b6b03"
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { auth, db };
+export const db = getFirestore(app);
